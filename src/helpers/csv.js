052801be8b4437ffd8b7554033761eb4a25e9ab2
Yegor G <yegorich_555@mail.ru>
2019-10-18 15:20:10 +0300

    Helpers. Add CSV support

--- src/helpers/csv.js
+++ src/helpers/csv.js
@@ -1,7 +1,73 @@
+const defLineSplitter = "\r"; // or /[\r\n|\r|\n]/
+const defValueSplitter = "\t"; // tab-key
+
 const CSV = {
-  splitter: ";",
-  parse: text => {},
-  stringify: obj => {}
+  parse: (
+    text,
+    lineSplitter = defLineSplitter,
+    valueSplitter = defValueSplitter
+  ) => {
+    if (!text) {
+      return null;
+    }
+    const lines = text.split(lineSplitter);
+    if (!lines.length) {
+      return text;
+    }
+    const keys = lines[0].split(valueSplitter);
+    const arr = new Array(lines.length - 1);
+    for (let i = 1; i < lines.length; ++i) {
+      const values = lines[i].split(valueSplitter);
+      const obj = {};
+      keys.forEach((key, k) => {
+        obj[key] = values[k]; // TODO: ConvertFromString
+      });
+      arr[i - 1] = obj;
+    }
+    console.warn("read", arr);
+    return arr;
+  },
+  stringify: (
+    arr,
+    lineSplitter = defLineSplitter,
+    valueSplitter = defValueSplitter
+  ) => {
+    if (!arr || !arr.length) {
+      return "";
+    }
+
+    const header = [];
+    const lines = new Array(arr.length - 1);
+    arr.forEach((v, i) => {
+      const keys = Object.keys(v).sort((key1, key2) => {
+        // sort keys because it can be different
+        let i1 = header.indexOf(key1);
+        if (i1 === -1) {
+          i1 = header.length;
+          header.push(key1);
+        }
+        let i2 = header.indexOf(key2);
+        if (i2 === -1) {
+          i2 = header.length;
+          header.push(key2);
+        }
+        return i1 - i2;
+      });
+
+      lines[i] = "";
+      const lastInd = keys.length - 1;
+      for (let k = 0; k < lastInd; ++k) {
+        lines[i] += v[keys[k]] + valueSplitter;
+      }
+      lines[i] = v[keys[lastInd]];
+    });
+
+    const line1 = header.join(valueSplitter);
+    const str = line1 + lineSplitter + lines.join(valueSplitter);
+
+    console.warn("write", str);
+    return str;
+  }
 };
 
 export default CSV;
