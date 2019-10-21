d8dba61850b69f4c81727acf89e91d3add5b97ce
Yegor G <yegorich_555@mail.ru>
2019-10-21 17:17:43 +0300

    Elements. DataTableEdit. Add copy logic

--- src/elements/dataTableEdit/index.jsx
+++ src/elements/dataTableEdit/index.jsx
@@ -2,7 +2,8 @@ import styles from "./style.scss";
 import Modal from "../modal";
 import PrimaryBtn from "../buttons/primaryBtn";
 import SecondaryBtn from "../buttons/secondaryBtn";
-import DataTable from "../dataTable";
+import DataTable, { DataTableHelper } from "../dataTable";
+import WarningBtn from "../buttons/warningBtn";
 
 const askTypes = {
   paste: 1,
@@ -19,6 +20,31 @@ function getRusSuf(length) {
       return "и";
     default:
       return "";
+  }
+}
+
+// eslint-disable-next-line no-unused-vars
+function selectElementContents(el) {
+  const { body } = document;
+  let range;
+  let sel;
+  if (document.createRange && window.getSelection) {
+    range = document.createRange();
+    sel = window.getSelection();
+    sel.removeAllRanges();
+    try {
+      range.selectNodeContents(el);
+      sel.addRange(range);
+    } catch (e) {
+      range.selectNode(el);
+      sel.addRange(range);
+    }
+    document.execCommand("copy");
+  } else if (body.createTextRange) {
+    range = body.createTextRange();
+    range.moveToElementText(el);
+    range.select();
+    range.execCommand("Copy");
   }
 }
 
@@ -63,7 +89,24 @@ export default class DataTableEdit extends DataTable {
   };
 
   onCopy = e => {
-    console.warn("copy", e.clipboardData);
+    const valueSeparator = "\t";
+    const lineSeparator = "\r\n";
+    const items = this.sort();
+
+    const rows = new Array(items.length);
+    rows[0] = this.headerKeys
+      .map(h => DataTableHelper.getHeaderText(h, h.text))
+      .join(valueSeparator);
+
+    items.forEach((item, i) => {
+      rows[i + 1] = this.headerKeys
+        .map(h => DataTableHelper.getCellValue(h, item[h.propName]))
+        .join(valueSeparator);
+    });
+
+    const txt = rows.join(lineSeparator);
+
+    navigator.clipboard.writeText(txt); // TODO it can be execCommand
   };
 
   onKeyDown = e => {
@@ -96,7 +139,10 @@ export default class DataTableEdit extends DataTable {
       onPaste: this.onPaste,
       onKeyDown: this.onKeyDown,
       onCopy: this.onCopy,
-      tabIndex: "-1"
+      tabIndex: "-1",
+      ref: el => {
+        this.refEl = el;
+      }
     };
   }
 
@@ -123,14 +169,24 @@ export default class DataTableEdit extends DataTable {
         ) : null}
         <div className={styles.btnGroup}>
           {navigator.clipboard ? (
-            <PrimaryBtn
-              onClick={() => {
-                navigator.clipboard.readText().then(this.paste);
-              }}
-            >
-              Вставить
-            </PrimaryBtn>
+            <>
+              <PrimaryBtn
+                onClick={() => {
+                  navigator.clipboard.readText().then(this.paste);
+                }}
+              >
+                Вставить
+              </PrimaryBtn>
+              <PrimaryBtn onClick={this.onCopy}>Копировать</PrimaryBtn>
+            </>
           ) : null}
+
+          <WarningBtn
+            onClick={this.onRemove}
+            disabled={!this.state.currentItem}
+          >
+            Удалить
+          </WarningBtn>
         </div>
       </>
     );
