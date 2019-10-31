6089d4f9cd4227d1e759d4e8e73167af85c2fbfb
Yegor G <yegorich_555@mail.ru>
2019-10-31 10:52:00 +0300

    Elements. DataTable. Add title to header

--- src/elements/dataTable/index.jsx
+++ src/elements/dataTable/index.jsx
@@ -138,6 +138,7 @@ export default class DataTable extends Component {
                     ]
                   : null
               }
+              title={item.title}
             >
               {text}
             </div>
